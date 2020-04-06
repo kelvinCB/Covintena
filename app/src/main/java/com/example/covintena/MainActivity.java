44e015ea8a6694e0aeb77e9245275ad9d604e82e
TitoMatos <titomatos-0@hotmail.com>
2020-04-06 14:30:50 -0400

    Estilo del Splash Screen y  la Pantalla de Inicio

--- app/src/main/java/com/example/covintena/MainActivity.java
+++ app/src/main/java/com/example/covintena/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide();
+
 
 
         btnInfo = (Button)findViewById(R.id.button);
