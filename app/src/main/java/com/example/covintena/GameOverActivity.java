f6b3d0956934f71b023f858b13891753eb254ca5
TitoMatos <titomatos-0@hotmail.com>
2020-04-08 14:44:02 -0400

    Estilo Game Over Flappy Virus

--- app/src/main/java/com/example/covintena/GameOverActivity.java
+++ app/src/main/java/com/example/covintena/GameOverActivity.java
@@ -2,13 +2,40 @@ package com.example.covintena;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameOverActivity extends AppCompatActivity {
+
+    Button btnvj, btnsalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over2);
+
+        btnvj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer sb = MediaPlayer.create(GameOverActivity.this, R.raw.sonidoboton);
+                sb.start();
+                Intent intent = new Intent(GameOverActivity.this, ContenedorFlappyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnsalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer sb = MediaPlayer.create(GameOverActivity.this, R.raw.sonidoboton);
+                sb.start();
+                Intent intent = new Intent(GameOverActivity.this, GamesActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
